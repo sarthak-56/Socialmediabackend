5048705bc5a090c2359d715f49daedd0d8bdd5d7
Sarthak Ranjan Mishra <125554009+sarthak-56@users.noreply.github.com>
2025-02-07 22:49:46 +0530

    asgi.py

--- Project/asgi.py
+++ Project/asgi.py
@@ -1,19 +1,8 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-import app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                app.routing.websocket_urlpatterns
-            )
-        )
-    ),
-})
+application = get_asgi_application()
